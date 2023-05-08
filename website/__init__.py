60de9a488c2d00b011064bedcb2866ed02422a37
kausikamanivannan <122924298+kausikamanivannan@users.noreply.github.com>
2023-05-08 08:13:26 -0400

    Add files via upload

--- website/__init__.py
+++ website/__init__.py
@@ -11,6 +11,7 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = "helloworld"
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
 
     from .views import views
@@ -30,11 +31,12 @@ def create_app():
     @login_manager.user_loader
     def load_user(id):
         return User.query.get(int(id))
-
+    
     return app
 
-
 def create_database(app):
-    if not path.exists("website/" + DB_NAME):
-        db.create_all(app=app)
-        print("Created database!")
+    if not path.exists(DB_NAME):
+        with app.app_context():
+            db.create_all()
+            print("Created database!")
+
